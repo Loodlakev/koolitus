27c3ac331265bcd2597e8db2b111d768d23b0df6
Loodlakev <101732199+Loodlakev@users.noreply.github.com>
2022-04-27 17:04:39 +0300

    kodutöö loeng12

---
+++ loeng12/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+app.post('/api/add', (req, res) => {
+  console.log('minu pihta toimub päring', req.body);
+  res.header({
+    'Content-Type': 'application/json',
+  });
+  res.json({ answer: 0 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
